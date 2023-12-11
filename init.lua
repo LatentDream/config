6f9d49157f492723ceb92bb161ce273505ea8ec9
guthi1 <guillaume.thibault.98@gmail.com>
2023-12-11 09:12:13 -0500

    Adding Copilot extension

--- init.lua
+++ init.lua
@@ -216,6 +216,9 @@ require('lazy').setup({
   {
     'ThePrimeagen/harpoon'
   },
+  {
+    'github/copilot.vim'
+  }
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
