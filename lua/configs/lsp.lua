e81ccb6d70dbe287183a35cfe8ef440c2a6fd8f8
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-01-28 14:37:05 -0500

    C multiple encoding problem Fix

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -122,3 +122,28 @@ require'lspconfig'.pylsp.setup{
    }
 }
 
+-- Clangd: Fix encoding problem
+local lspconfig = require("lspconfig.configs")
+local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+local clangd_capabilities = cmp_capabilities
+clangd_capabilities.textDocument.semanticHighlighting = true
+clangd_capabilities.offsetEncoding = "utf-8"
+
+lspconfig.clangd.setup{
+    capabilities = clangd_capabilities,
+    cmd = {
+    	"clangd",
+    	"--background-index",
+    	"--pch-storage=memory",
+    	"--clang-tidy",
+    	"--suggest-missing-includes",
+    	"--cross-file-rename",
+    	"--completion-style=detailed",
+    },
+    init_options = {
+    	clangdFileStatus = true,
+    	usePlaceholders = true,
+    	completeUnimported = true,
+    	semanticHighlighting = true,
+    }
+}
