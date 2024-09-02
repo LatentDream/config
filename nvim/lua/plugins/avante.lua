7431da8eb5369cf371f8f8a835ccf91c43558f3b
latentdream <guillaume.thibault.98@proton.me>
2024-09-01 22:15:52 -0400

    chore(nvim): add avante

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,47 @@
+return {
+    {
+        -- https://github.com/yetone/avante.nvim
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        opts = {
+            provider = "claude",
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-3-5-sonnet-20240620",
+                temperature = 0,
+                max_tokens = 4096,
+            },
+        },
+        build = ":AvanteBuild",
+        dependencies = {
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+            {
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
+    }
+}
