0f576b1b5b764572da7e1d0a360c6352e9c79c25
latentdream <guillaume.thibault.98@proton.me>
2024-09-06 23:24:59 -0400

    chore(nvim): `rm rf toggleterm`

--- nvim/lua/configs/toggleterm.lua
+++ nvim/lua/configs/toggleterm.lua
@@ -1,83 +1,31 @@
-local status_ok, toggleterm = pcall(require, "toggleterm")
-if not status_ok then
-	return
+lazygit_term = nil
+function lazygit_toggle()
+    if vim.fn.executable('lazygit') == 1 then
+        if lazygit_term and vim.api.nvim_buf_is_valid(lazygit_term) then
+            -- If lazygit is already running, close it
+            vim.api.nvim_buf_delete(lazygit_term, {force = true})
+            lazygit_term = nil
+        else
+            -- Open lazygit in a new full-screen buffer
+            vim.cmd('enew')
+            lazygit_term = vim.api.nvim_get_current_buf()
+            vim.fn.termopen("lazygit", {
+                on_exit = function()
+                    if vim.api.nvim_buf_is_valid(lazygit_term) then
+                        vim.api.nvim_buf_delete(lazygit_term, {force = true})
+                    end
+                    lazygit_term = nil
+                end
+            })
+            -- Hide the buffer from the buffer list and make it disposable
+            vim.api.nvim_buf_set_option(lazygit_term, 'buflisted', false)
+            vim.api.nvim_buf_set_option(lazygit_term, 'bufhidden', 'wipe')
+            vim.cmd('startinsert')
+        end
+    else
+        print("lazygit is not installed")
+    end
 end
 
-toggleterm.setup({
-	size = 65,
-	open_mapping = [[<c-\>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 6,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-		},
-		title_pos= "left"
-	},
-})
-
-function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-end
-
-
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
-local Terminal = require("toggleterm.terminal").Terminal
-
-local node = Terminal:new({ cmd = "node", hidden = true })
-
-function _NODE_TOGGLE()
-	node:toggle()
-end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-	htop:toggle()
-end
-
-local python = Terminal:new({ cmd = "python", hidden = true })
-
-function _PYTHON_TOGGLE()
-	python:toggle()
-end
-
-
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
-end
-
-local spotify = Terminal:new({ cmd = "spt", hidden = true })
-
-function _SPOTIFY_TOGGLE()
-	spotify:toggle()
-end
-
-
-vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>tt', "<cmd>1ToggleTerm direction=float<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>t1', "<cmd>2ToggleTerm direction=vertical<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>t2', "<cmd>3ToggleTerm direction=vertical<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>tp", "<cmd>lua _PYTHON_TOGGLE()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>th", "<cmd>lua _HTOP_TOGGLE()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>tn", "<cmd>lua _NODE_TOGGLE()<CR>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>ts", "<cmd>lua _SPOTIFY_TOGGLE()<CR>", {noremap = true, silent = true})
-
+-- Set the mapping
+vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>lua lazygit_toggle()<CR>', { noremap = true, silent = true })
