e9fad18b6bae65aa27c8697a3d65647f981466ef
latentdream <guillaume.thibault.98@proton.me>
2024-10-20 16:38:46 -0400

    feat(nvim): SHIFT+H SHIFT+L to go next/prev harpoon buffer

--- nvim/lua/configs/harpoon.lua
+++ nvim/lua/configs/harpoon.lua
@@ -20,10 +20,23 @@
 -- Harpoon 1 config fall back | Delete this if you use Harpoon 2
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
+-- Your existing keybindings
 vim.keymap.set("n", "<leader>j", mark.add_file, { desc = '[H]arpoon [A]add file'})
 vim.keymap.set("n", "<leader><space>", ui.toggle_quick_menu, { desc = '[H]arpoon Explorer'})
 vim.keymap.set("n", "<leader>hq", function() ui.nav_file(1) end, { desc = '[H]arpoon 1'})
 vim.keymap.set("n", "<leader>hw", function() ui.nav_file(2) end, { desc = '[H]arpoon 2'})
 vim.keymap.set("n", "<leader>he", function() ui.nav_file(3) end, { desc = '[H]arpoon 3'})
 vim.keymap.set("n", "<leader>hr", function() ui.nav_file(4) end, { desc = '[H]arpoon 4'})
+
+-- Add navigation with Shift+H and Shift+L
+vim.keymap.set("n", "H", function()
+    -- Navigate to previous file in list
+    ui.nav_prev()
+end, { desc = 'Harpoon Previous' })
+
+vim.keymap.set("n", "L", function()
+    -- Navigate to next file in list
+    ui.nav_next()
+end, { desc = 'Harpoon Next' })
