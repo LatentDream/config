23a892c0be194fe028c6cbd27774fb83a9446ffc
Latent <guillaume.thibault.98@proton.me>
2024-10-28 21:18:38 -0400

    chore(nvim): removing new `auto_suggest` which is on by default

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -12,6 +12,13 @@ return {
                 temperature = 0,
                 max_tokens = 4096,
             },
+            behaviour = {
+                auto_suggestions = false,
+                auto_set_highlight_group = true,
+                auto_set_keymaps = true,
+                auto_apply_diff_after_generation = false,
+                support_paste_from_clipboard = false,
+            },
         },
         build = ":AvanteBuild",
         dependencies = {
