b3ea39e7be53d081051a1694d12a91db52168841
Latent <guillaume.thibault.98@proton.me>
2025-01-05 17:06:17 -0500

    revert(nvim): `cmp` -> `blink`
    This reverts commit 1a7d23c95f1eb074cd0ce603cf1ea980dc21a2e2.
    
    Revert "chore(nvim): `cmp` -> `blink`"
    
    This reverts commit 6f56608b578b1100d526a755c6091d091a1d963d.

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -1,11 +1,54 @@
 -- [[ Configure LSP ]]
--- Main config are in ../configs/lsp.lua
 
 -- Some utilities
-vim.keymap.set("n", "<leader>ls", ':LspStart<cr>', { desc = '[L]sp [s]tart' })
-vim.keymap.set("n", "<leader>lp", ':LspStop<cr>', { desc = '[L]sp [p]pause' })
-vim.keymap.set("n", "<leader>lr", ':LspRestart<cr>', { desc = '[L]sp [r]estart' })
+vim.keymap.set("n", "<leader>ls", ':LspStart<cr>', { desc = '[L]sp [s]tart'})
+vim.keymap.set("n", "<leader>lp", ':LspStop<cr>', { desc = '[L]sp [p]pause'})
+vim.keymap.set("n", "<leader>lr", ':LspRestart<cr>', { desc = '[L]sp [r]estart'})
+
+
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+  -- Function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', require('fzf-lua').lsp_definitions, '[G]oto [D]efinition')
+  nmap('gr', require('fzf-lua').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', require('fzf-lua').lsp_implementations, '[G]oto [I]mplementation')
+  nmap('<leader>D', require('fzf-lua').lsp_typedefs, 'Type [D]efinition')
+  nmap('<leader>ds', require('fzf-lua').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('fzf-lua').lsp_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>ca', require('fzf-lua').lsp_code_actions, '[C]ode [A]ction')
+
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Lesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+end
+
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Format<CR>', { noremap = true, silent = true })
+
 
 -- document existing key chains
 local wk = require("which-key")
@@ -32,5 +75,71 @@ wk.add({
   { "<leader>w_", hidden = true },
   { "<leader>x",  group = "[X]Error" },
   { "<leader>x_", hidden = true },
-  { "<leader>",   group = "VISUAL <leader>", mode = "v" },
+  { "<leader>", group = "VISUAL <leader>", mode = "v" },
 })
+
+-- mason-lspconfig requires that these setup functions are called in this order
+-- before setting up the servers.
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+-- Enable the following language servers
+--  Add any additional override configuration in the following tables. They will be passed to
+--  the `settings` field of the server config.
+--  If you want to override the default filetypes that your language server will attach to you can
+--  define the property 'filetypes' to the map in question.
+local servers = {
+  -- TODO:
+  clangd = {},
+  -- gopls = {},
+  -- pyright = {},
+  rust_analyzer = {},
+  -- tsserver = {},
+  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+      diagnostics = { disable = { 'missing-fields' } },
+    },
+  },
+}
+
+-- Setup neovim lua configuration
+require('neodev').setup()
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+      filetypes = (servers[server_name] or {}).filetypes,
+    }
+  end,
+}
+
+-- require 'lspconfig'.pylsp.setup {
+--   settings = {
+--     pylsp = {
+--       plugins = {
+--         pycodestyle = {
+--           ignore = { 'E501', 'W391', 'W293', 'E241' },
+--           maxLineLength = 120
+--         }
+--       }
+--     }
+--   }
+-- }
