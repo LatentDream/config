92e0eab6fa3c08acbd5d9a0ffff4f7e12123b634
Latent <guillaume.thibault.98@proton.me>
2025-01-10 18:13:45 -0500

    chore(nvim): terminal keymap

--- nvim/lua/configs/keymap.lua
+++ nvim/lua/configs/keymap.lua
@@ -1,14 +1,18 @@
-
 -- nvim/lua/configs/keymap.lua
 
 -- Resize window using <Ctrl> arrow keys
-vim.api.nvim_set_keymap('n', '<C-Up>', ':resize +2<CR>', { noremap = true, silent = true, desc = 'Increase window height' })
-vim.api.nvim_set_keymap('n', '<C-Down>', ':resize -2<CR>', { noremap = true, silent = true, desc = 'Decrease window height' })
-vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true, desc = 'Decrease window width' })
-vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true, desc = 'Increase window width' })
+vim.api.nvim_set_keymap('n', '<C-Up>', ':resize +2<CR>',
+    { noremap = true, silent = true, desc = 'Increase window height' })
+vim.api.nvim_set_keymap('n', '<C-Down>', ':resize -2<CR>',
+    { noremap = true, silent = true, desc = 'Decrease window height' })
+vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>',
+    { noremap = true, silent = true, desc = 'Decrease window width' })
+vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>',
+    { noremap = true, silent = true, desc = 'Increase window width' })
 
 
-vim.api.nvim_set_keymap('n', '<leader>th', ':set invhlsearch hlsearch?<CR>', { noremap = true, silent = true, desc = '[T]oggle [H]ighlight Search' })
+vim.api.nvim_set_keymap('n', '<leader>th', ':set invhlsearch hlsearch?<CR>',
+    { noremap = true, silent = true, desc = '[T]oggle [H]ighlight Search' })
 
 -- Custom insert
 function insertArrow()
@@ -22,5 +26,18 @@ vim.api.nvim_set_keymap('n', '<C-s>', ':w', { noremap = true, silent = true, des
 vim.api.nvim_set_keymap('n', '<C-S>', ':aw', { noremap = true, silent = true, desc = 'Save all files' })
 
 -- small c conveniance gh to go to header
-vim.api.nvim_set_keymap('n', 'gh', ':ClangdSwitchSourceHeader<CR>', { noremap = true, silent = true, desc = '[G]o to [h]eader or source' })
+vim.api.nvim_set_keymap('n', 'gh', ':ClangdSwitchSourceHeader<CR>',
+    { noremap = true, silent = true, desc = '[G]o to [h]eader or source' })
 
+-- Normal esc in terminal mode
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
+
+-- Enter terminal in insert mode automatically
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = 'Exit terminal and close buffer' })
+
+-- Open terminal and enter insert mode
+vim.keymap.set('n', '<leader>tt', ':bel term<CR>i', {
+    noremap = true,
+    silent = true,
+    desc = '[T]oggle build-in [T]erminal'
+})
