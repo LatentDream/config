59b51da6c5b16b8449a120bbadfb2a8f1d3901f2
LatentDream <guillaume.thibault.98@proton.me>
2025-08-23 12:17:26 -0400

    chore(nvim): remove avantee

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -12,58 +12,6 @@ end
 if check_user() ~= "guillaume.thibault" then
     return {
         {
-            -- https://github.com/yetone/avante.nvim
-            "yetone/avante.nvim",
-            event = "VeryLazy",
-            lazy = false,
-            opts = {
-                provider = "claude",
-                claude = {
-                    endpoint = "https://api.anthropic.com",
-                    model = "claude-3-5-sonnet-20240620",
-                    temperature = 0,
-                    max_tokens = 4096,
-                },
-                behaviour = {
-                    auto_suggestions = false,
-                    auto_set_highlight_group = true,
-                    auto_set_keymaps = true,
-                    auto_apply_diff_after_generation = false,
-                    support_paste_from_clipboard = false,
-                },
-            },
-            build = ":AvanteBuild",
-            dependencies = {
-                "stevearc/dressing.nvim",
-                "nvim-lua/plenary.nvim",
-                "MunifTanjim/nui.nvim",
-                "nvim-tree/nvim-web-devicons",
-                {
-                    "HakonHarnes/img-clip.nvim",
-                    event = "VeryLazy",
-                    opts = {
-                        -- recommended settings
-                        default = {
-                            embed_image_as_base64 = false,
-                            prompt_for_file_name = false,
-                            drag_and_drop = {
-                                insert_mode = true,
-                            },
-                            -- required for Windows users
-                            use_absolute_path = true,
-                        },
-                    },
-                },
-                {
-                    'MeanderingProgrammer/render-markdown.nvim',
-                    opts = {
-                        file_types = { "markdown", "Avante" },
-                    },
-                    ft = { "markdown", "Avante" },
-                },
-            },
-        },
-        {
             "supermaven-inc/supermaven-nvim",
             config = function()
                 require("supermaven-nvim").setup({
